ca73048875c8fe593405f4b0646ba2aa05d918cb
glennwester <glenn.wester@gmail.com>
2019-06-10 13:13:13 -0700

    Create a new rectangle

---
+++ main.js
@@ -0,0 +1,23 @@
+// [1]
+const { Rectangle, Color } = require("scenegraph");
+
+// [2]
+function rectangleHandlerFunction(selection) {
+  // [3]
+  const newElement = new Rectangle();
+  newElement.width = 100;
+  newElement.height = 50;
+  newElement.fill = new Color("Purple");
+
+  // [4]
+  selection.insertionParent.addChild(newElement);
+  // [5]
+  newElement.moveInParentCoordinates(100, 100);
+}
+
+// [6]
+module.exports = {
+  commands: {
+    createRectangle: rectangleHandlerFunction
+  }
+};
